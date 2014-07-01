6969e101dab97aa4a119bf15184c7ee91821e4a4
Hideyuki Tachibana <tachibana@hil.t.u-tokyo.ac.jp>
2014-07-01 20:22:19 +0900

    renamed the files, and added test5

---
+++ python/1_basic_grammar/test5_loadfile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import test1_basicgrammar as bg # import a file (test1_basicgrammar.py) in a same directory 
+
+"""
+Note: The letters of the file names (module names) should be lower case.
+The files that begin with a digit cannot be imported as a module (?)
+
+"""
+
+bg.func() # call a function "func()" in the file
+
+
