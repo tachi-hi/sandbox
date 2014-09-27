f3b1e930387df0c390e46c8df97ac1192d4faf17
Hideyuki Tachibana <tachibana@hil.t.u-tokyo.ac.jp>
2014-09-28 03:58:06 +0900

    added a memo

--- scala/grammar/test.scala
+++ scala/grammar/test.scala
@@ -1,5 +1,6 @@
 
 
+
 object test{
 	def main(args: Array[String]){
 
@@ -54,7 +55,47 @@ object test{
 		var myArrayDouble = myArray.foreach{e => e * 2} // This does not return the doubled array.
 		println(myArrayDouble)
 		//myArrayDouble.foreach{ e => println(e)}
+		
+		// This is achieved by "for ... yield" as follows
+		var myArrayTriple = for( e <- myArray ) yield e * 3
+		myArrayTriple.foreach{ e => println(e) }
+		
+		// Function (method of a class)
+		//-----------------------------------------------------------------------
+		println( myFunction(1,2) )
+		printMessage()
 
+		// Function (function object)
+		//-----------------------------------------------------------------------
+		var myFuncObj = (x:Int, y:Int) => (x + y)
+		// var myFuncObj : (Int, Int) => Int = (x:Int, y:Int) => (x + y) 
+		// ": (Int, Int) => Int" is the type of this function
+		println( myFuncObj(1,2) )
+
+		// Recursion requires the infomation on the type
+		// Recursive function cannot be defined as var nor val (?)
+		def Fibonacci : Int => BigInt = 
+		n => n match {
+				case i:Int if i < 1 => 0
+				case 1 | 2 => 1
+				case _:Int => Fibonacci( n - 1) + Fibonacci( n - 2 )
+			}
+
+		println( Fibonacci(1) )
+		println( Fibonacci(2) )
+		println( Fibonacci(3) )
+		println( Fibonacci(4) )
+		println( Fibonacci(5) )
+		println( Fibonacci(6) )
+	}
+
+	def myFunction(x:Int, y:Int):Int = 
+	{
+		return x + y // return is not necessary; scala automatically returns the last value
+	}
+	def printMessage():Unit =  
+	{
+		println("void")
 	}
 }
 
