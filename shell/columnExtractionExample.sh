802d2a6fc78e233f1744c2a4eebfd2e73722fabd
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2014-10-28 20:16:03 +0900

    added a memo on cut command

--- shell/columnExtractionExample.sh
+++ shell/columnExtractionExample.sh
@@ -1,1 +1,6 @@
 cat test.dat | cut -d " " -f 1,3
+# Notes:
+# The cut command does not regard the multiple spaces as an single delimeter. 
+# Ex: (_ is space)
+#   a_b_c -> a b c
+#   a___b_c -> a _ b c
