f187c1eb435ddd5a4afd9ae0761c917cd21a1d1f
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2016-04-21 16:52:08 +0900

    python can unpack list

---
+++ python/1_basic_grammar/lhs_unpack_list.py
@@ -0,0 +1,20 @@
+def fun(x):
+    return [x, x*2, x*3]
+
+# valid
+[x, y, z] = fun(3)
+
+print (x)
+print (y)
+print (z)
+
+# vaild
+x, y, z = fun(3)
+print (x)
+print (y)
+print (z)
+
+# invalid 
+x, y = fun(3)
+print (x)
+print (y)
